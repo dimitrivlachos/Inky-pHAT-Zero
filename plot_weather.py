4c705e19295a648d1b61585222da9f827c830907
Dimitri Vlachos <86776716+dimitrivlachos@users.noreply.github.com>
2023-03-26 23:22:32 +0000

    Improved graphing code

--- plot_weather.py
+++ plot_weather.py
@@ -33,12 +33,12 @@ def plot_data(df):
 
     # plot temperature, humidity and pressure on the same graph
     fig, ax = plt.subplots(1, 1, figsize=((212/dpi), (122/dpi)), sharex=True, dpi=dpi)
-    ax.plot(df['timestamp'], df['temperature'], 'r')
+    ax.plot(df['timestamp'], df['temperature'], '#ff8282')
     ax.plot(df['timestamp'], df['humidity'], 'b')
 
     # plit pressure on a second y-axis
     ax2 = ax.twinx()
-    ax2.plot(df['timestamp'], df['pressure'], 'g')
+    ax2.plot(df['timestamp'], df['pressure'], '#ff0000')
 
     ax.spines[['right', 'top', 'left', 'bottom']].set_visible(False)
     ax2.spines[['right', 'top', 'left', 'bottom']].set_visible(False)
@@ -47,15 +47,18 @@ def plot_data(df):
     ax.set_yticks([])
     ax2.set_yticks([])
 
+    # Set xlim to the minimum and maximum values of the timestamp column
+    ax.set_xlim(df['timestamp'].min(), df['timestamp'].max())
+
     # Make the graph fill the whole figure
     plt.tight_layout()
     
     # Save plot to file
-    plt.savefig('weather.png')
+    plt.savefig('weather.png', bbox_inches='tight', pad_inches=0)
 
 df = read_csv('weather.csv')
 
 # Select data only within the last 24 hours
-df = df[df['timestamp'] > pd.Timestamp.now() - pd.Timedelta('200 hours')]
+#df = df[df['timestamp'] > pd.Timestamp.now() - pd.Timedelta('24 hours')]
 
 plot_data(df)
