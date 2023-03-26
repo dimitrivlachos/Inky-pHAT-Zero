81d8e2fa776c879ed39c024e9cd78cde678e9a86
Dimitri Vlachos <86776716+dimitrivlachos@users.noreply.github.com>
2023-03-26 22:08:38 +0000

    Updated name

--- b.py
+++ b.py
@@ -1,13 +1,13 @@
 '''
 Converts a string to a binary file.
-Usage: python text_to_bin.py <input_string> <file_name>
-Example: python text_to_bin.py "Hello World" hello.bin
+Usage: python b.py <input_string> <file_name>
+Example: python b.py "Hello World" hello.bin
 '''
 
 import sys
 
 if len(sys.argv) != 3:
-    print("Usage: python binary_writer.py <input_string> <file_name>")
+    print("Usage: python b.py <input_string> <file_name>")
     sys.exit(1)
 
 input_string = sys.argv[1]
