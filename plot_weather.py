8d391700e4492a3eb3c0082a2b5d10ddcaab3d4b
Dimitri Vlachos <86776716+dimitrivlachos@users.noreply.github.com>
2023-03-26 23:24:38 +0000

    Further graphing refinements

--- plot_weather.py
+++ plot_weather.py
@@ -17,28 +17,14 @@ def read_csv(filename):
 def plot_data(df):
     dpi = 120
 
-    #fig, ax = plt.subplots(3, 1, figsize=((212/dpi), (122/dpi)), sharex=True, dpi=dpi)
-    # ax[0].plot(df['timestamp'], df['temperature'], 'r')
-    # ax[0].set_ylabel('Temperature (C)')
-    # ax[1].plot(df['timestamp'], df['humidity'], 'b')
-    # ax[1].set_ylabel('Humidity (%)')
-    # ax[2].plot(df['timestamp'], df['pressure'], 'g')
-    # ax[2].set_ylabel('Pressure (hPa)')
-    # # format x-axis ticks as dates
-    # ax[2].xaxis.set_major_formatter(mdates.DateFormatter('%d/%m'))
-    # # angle x-axis labels
-    # plt.setp(ax[2].xaxis.get_majorticklabels(), rotation=45)
-    # # set x-axis ticks every day
-    # ax[2].xaxis.set_major_locator(mdates.DayLocator())
-
     # plot temperature, humidity and pressure on the same graph
     fig, ax = plt.subplots(1, 1, figsize=((212/dpi), (122/dpi)), sharex=True, dpi=dpi)
-    ax.plot(df['timestamp'], df['temperature'], '#ff8282')
-    ax.plot(df['timestamp'], df['humidity'], 'b')
+    ax.plot(df['timestamp'], df['temperature'], 'r')
+    ax.plot(df['timestamp'], df['humidity'], 'black')
 
     # plit pressure on a second y-axis
     ax2 = ax.twinx()
-    ax2.plot(df['timestamp'], df['pressure'], '#ff0000')
+    ax2.plot(df['timestamp'], df['pressure'], 'r')
 
     ax.spines[['right', 'top', 'left', 'bottom']].set_visible(False)
     ax2.spines[['right', 'top', 'left', 'bottom']].set_visible(False)
@@ -51,7 +37,7 @@ def plot_data(df):
     ax.set_xlim(df['timestamp'].min(), df['timestamp'].max())
 
     # Make the graph fill the whole figure
-    plt.tight_layout()
+    #plt.tight_layout()
     
     # Save plot to file
     plt.savefig('weather.png', bbox_inches='tight', pad_inches=0)
